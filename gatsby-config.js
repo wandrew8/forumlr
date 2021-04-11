e53f962c939121a8e35172b5af9c462aea8480ef
Andrew Weiss <wandrew8@gmail.com>
2021-04-11 13:13:14 -0700

    adds gatsby-source-strapi to connect to deployed backend

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,9 +28,15 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-source-strapi`,
+      options: {
+        apiURL: `https://shrouded-tundra-14519.herokuapp.com`,
+        queryLimit: 100, // Default to 100
+        contentTypes: [`blog-post`, `tags`],
+      },
+    },
     `gatsby-plugin-gatsby-cloud`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    
   ],
 }
